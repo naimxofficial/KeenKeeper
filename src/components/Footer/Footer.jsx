24e90b1333b67c8bd86590c04c48f87b073634ee
Naim Uddin <naimxofficial.bd@gmail.com>
2026-04-14 13:25:30 +0600

    minor change on the footer section and cloudeflare pages deploy build test

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -5,7 +5,7 @@ const Footer = () => {
     return (
         <div className='text-center p-10 '>
             <div className='flex flex-col gap-3 justify-center items-center py-10'>
-            <img className='not-md:w-1/2' src={'KeenKeeperFooter.svg'} alt="keenkeeper footer logo"  />
+            <img className='not-md:w-4/5' src={'KeenKeeperFooter.svg'} alt="keenkeeper footer logo"  />
             <p className='not-md:text-xs opacity-80'>Your personal shelf of meaningful connections. Browse, tend, and nurture the relationships that matter most.</p>
             <h2 className='font-medium text-xl'>Social Links</h2>
             <ul className='flex items-center gap-3'>
