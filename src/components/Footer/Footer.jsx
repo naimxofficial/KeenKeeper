ce7b0759fc62a7c190d99c72d5bf86f2a226a4c2
Naim Uddin <naimxofficial.bd@gmail.com>
2026-04-14 13:41:28 +0600

    changes on redirects file to fix deploy on claudeflare pages

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -6,7 +6,7 @@ const Footer = () => {
         <div className='text-center p-10 '>
             <div className='flex flex-col gap-3 justify-center items-center py-10'>
             <img className='not-md:w-4/5' src={'KeenKeeperFooter.svg'} alt="keenkeeper footer logo"  />
-            <p className='not-md:text-xs opacity-80'>Your personal shelf of meaningful connections. Browse, tend, and nurture the relationships that matter most..</p>
+            <p className='not-md:text-xs opacity-80'>Your personal shelf of meaningful connections. Browse, tend, and nurture the relationships that matter most.</p>
             <h2 className='font-medium text-xl'>Social Links</h2>
             <ul className='flex items-center gap-3'>
                 <li><Link to={"https://www.instagram.com/"}><img src={"Instagram.svg"} alt="instagram icon" /></Link></li>
