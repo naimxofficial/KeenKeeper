a802d37f830e3f2e05fe897f42772f1ccdc652f7
Naim Uddin <naimxofficial.bd@gmail.com>
2026-04-14 17:39:12 +0600

    Designed HomePage and added friends cards on the Homepage

---
+++ src/components/ui/FriendCard.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+
+const FriendCard = ({friend}) => {
+    console.log(friend);
+    const {id, picture ,name, days_since_contact , tags, status} = friend;
+    return (
+        <div className='p-6 bg-white  text-center flex flex-col items-center justify-center gap-2 rounded-lg drop-shadow-sm'>
+            <img className='mb-1 rounded-full' src={picture} alt="" />
+            <h3 className='font-semibold text-xl'>{name}</h3>
+            <p className='text-[#64748B] text-xs'>{days_since_contact}d ago</p>
+            <div>
+
+            {
+                tags.map((tag, index)=> <div tag={tag} key={index} className='badge mr-2 text-[#244D3F] bg-[#CBFADB] rounded-full p-4 font-semibold text-xs'>{tag}</div>)
+            }
+            </div>
+            <p className={`text-xs font-semibold px-3 py-2 text-white rounded-full ${status === "overdue" ? 'bg-[#EF4444]' : status === "on-track" ? 'bg-[#244D3F]' : "bg-[#EFAD44]"}`}>{status}</p>
+            
+        </div>
+    );
+};
+
+export default FriendCard;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,56 @@
-import React from 'react';
+import React, { use } from 'react';
+import FriendCard from '../components/ui/FriendCard';
+const friendsPromise = fetch('friends.json').then(res => res.json());
 
 const Home = () => {
+    const friends = use(friendsPromise);
+    
     return (
-        <div>
-            Home
+        <div className='py-20 w-11/12 mx-auto'>
+
+            {/* friends to keep close */}
+            <div className='text-center'>
+                <h2 className='mb-4 font-bold text-5xl not-md:text-2xl'>Friends to keep close in your life</h2>
+                <p className='text-[#64748B] not-md:text-xs mb-8'>Your personal shelf of meaningful connections. Browse, tend, and nurture the <br className='not-md:hidden' />
+                    relationships that matter most.</p>
+                    <button className='btn bg-[#244D3F] font-semibold text-white'>+ Add a Friend</button>
+            </div>
+
+            {/* Stats */}
+            <div className='text-center py-10 border-b border-[#E9E9E9] gap-6 grid grid-cols-2 md:grid-cols-2 lg:grid-cols-4'>
+
+                <div className='bg-white rounded-lg drop-shadow-sm py-8 space-y-2 '>
+                    <h2 className='font-semibold text-[32px] text-[#244D3F]'>{friends.length}</h2>
+                    <p className='text-[#64748B] text-[18px] not-md:text-sm'>Total Friends</p>
+                </div>
+
+                <div className='bg-white rounded-lg drop-shadow-sm  py-8  space-y-2'>
+                    <h2 className='font-semibold text-[32px] text-[#244D3F]'>{friends.filter(f => f.status === "on-track").length}</h2>
+                    <p className='text-[#64748B] text-[18px] not-md:text-sm'>On Track</p>
+                </div>
+
+                <div className='bg-white rounded-lg drop-shadow-sm  py-8 space-y-2'>
+                    <h2 className='font-semibold text-[32px] text-[#244D3F]'>{friends.filter(f => f.status === "overdue").length}</h2>
+                    <p className='text-[#64748B] text-[18px] not-md:text-sm'>Need Attention</p>
+                </div>
+
+                <div className='bg-white  rounded-lg drop-shadow-sm py-8  space-y-2'>
+                    <h2 className='font-semibold text-[32px] text-[#244D3F]'>7</h2>
+                    <p className='text-[#64748B] text-[18px] not-md:text-sm'>Interactions This Month</p>
+                </div>
+
+            </div>
+
+            {/* Your Friends */}
+            <div className='mt-10'>
+                <h2 className='font-semibold mb-4 text-2xl'>Your Friends</h2>
+
+                <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-6'>
+                    {
+                        friends.map(friend => <FriendCard friend={friend} key={friend.id}></FriendCard>)
+                    }
+                </div>
+            </div>
         </div>
     );
 };
